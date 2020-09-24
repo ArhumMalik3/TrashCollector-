049e5827a8c5a009d794a966ce3de1dd170b9cc5
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-09-24 00:27:04 -0500

    Completed functionality for the employees default view to show a list of the days trash pickups in the area. I switched the index view to take in a customer instead of an employee

--- TrashCollector/Controllers/EmployeesController.cs
+++ TrashCollector/Controllers/EmployeesController.cs
@@ -46,12 +46,15 @@ namespace TrashCollector.Controllers
                 RedirectToAction("Create");
             }
             //I need to do a query to find that user id is in the db if null the call create method if not then set that customer equal to this one
-
-            
             var today = DateTime.Today.DayOfWeek.ToString();
-            var zipCodesInArea = _context.Addresses.Where(a => a.zipCode == employee.zipCode); 
-            var applicationDbContext = _context.Employees.Include(e => e.IdentityUser);
-            return View(applicationDbContext);
+            List<Customer> customerPickUps = null;
+            List<Customer> customersInDb = GetCustomers();
+            var zipCodesInArea = customersInDb.SkipWhile(c => c.Address.zipCode != employee.zipCode).ToList();
+            
+            customerPickUps = zipCodesInArea.Where(c => c.weeklyPickUpDay.ToString() == today).ToList();
+             
+            
+            return View(customerPickUps);
         }
 
         // GET: Employees/Details/5
